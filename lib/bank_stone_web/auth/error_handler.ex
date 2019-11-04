05aed81c2e122564e1fd1c98e3f815bdc83d021a
Gustavo Oliveira <goliveira@localhost.localdomain>
2019-11-04 17:53:54 -0200

    update format

--- lib/bank_stone_web/auth/error_handler.ex
+++ lib/bank_stone_web/auth/error_handler.ex
@@ -5,9 +5,10 @@ defmodule BankStoneWeb.Auth.ErrorHandler do
   import Plug.Conn
 
   def auth_error(conn, {type, _reason}, _opts) do
-    IO.inspect to_string(type)
+    IO.inspect(to_string(type))
 
     body = Poison.encode!(%{error: to_string(type)})
+
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(401, body)
