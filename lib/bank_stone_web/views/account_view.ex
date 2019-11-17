f198a7bfd60807397906a8d29fab6d09794a2d69
Cosmarca <server.cosmarca@gmail.com>
2019-11-17 10:32:23 -0300

    update coverage

--- lib/bank_stone_web/views/account_view.ex
+++ lib/bank_stone_web/views/account_view.ex
@@ -5,10 +5,6 @@ defmodule BankStoneWeb.AccountView do
     %{data: render_many(accounts, __MODULE__, "account.json")}
   end
 
-  def render("show.json", %{account: account}) do
-    %{data: render_one(account, __MODULE__, "account.json")}
-  end
-
   def render("account.json", %{account: account}) do
     %{
       id: account.id,
