04582e983f4cf953dc8547b4661c822fe2273d5e
root <root@localhost.localdomain>
2019-11-17 11:46:19 -0200

    update config

--- config/test.exs
+++ config/test.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :bank_stone, BankStone.Repo,
-  username: "root",
+  username: "postgres",
   password: "1234",
   database: "bank_stone_test",
-  hostname: "database",
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
